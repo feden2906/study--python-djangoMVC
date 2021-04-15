d3680c0ea3dd8b23c875b1e2130f41b6b12836af
Fedenko Maksym <feden2906@gmail.com>
2021-04-15 23:30:07 +0300

    db

--- study__python_djangoMVC/settings.py
+++ study__python_djangoMVC/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'study__python_djangoMVC.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
